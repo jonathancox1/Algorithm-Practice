69aa20f51402ef116dd2d5b43d860c1995a4eac5
Jonathan Cox <cox.jonathan@gmail.com>
2020-06-28 21:54:59 -0400

    containsDuplicates

--- containsDuplicates.js
+++ containsDuplicates.js
@@ -1,6 +1,14 @@
 // Input: [1,2,3,1]
 // Output: true
 
-function containsDuplicates() {
-
+function containsDuplicates(arr) {
+    let array = []
+    for (i in arr) {
+        if (!array.includes(arr[i])) {
+            array.push(arr[i])
+        }
+    }
+    return !(arr.toString() == array.toString());
 }
+
+console.log(containsDuplicates([1, 2, 3, 4]));
